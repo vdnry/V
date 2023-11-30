b6dab51407cdfca86f7ce467b7f4e793acf20798
RealOrangeIND <realorangeind@gmail.com>
2023-11-30 23:33:04 +0530

    *IMPORTANT* Added algorithm and some UI changes

--- soilcalculator/npkScript.js
+++ soilcalculator/npkScript.js
@@ -1,13 +1,51 @@
 let userEmail;
 
+let state;
+let season;
+let landArea;
+let phInput;
+let moisture;
+let nitrogen;
+let phosphorous;
+let potassium;
+
+let idealNitrogen;
+let idealPhosphorous;
+let idealPotassium;
+
+let idealNitrogenForLand;
+let idealPhosphorousForLand;
+let idealPotassiumForLand;
+
+let totalNitrogen;
+let totalPhosphorous;
+let totalPotassium;
+
+let nitrogenDifference;
+let phosphorousDifference;
+let potassiumDifference;
+
+let nitrogenFertilizer;
+let phosphorousFertilizer;
+let potassiumFertilizer;
+
+const fertilizerCostNitrogen = 60;
+const fertilizerCostPhosphorous = 110;
+const fertilizerCostPotassium = 100;
+
+let totalNitrogenCost;
+let totalPhosphorousCost;
+let totalPotassiumCost;
+
+let finalTotalCost;
+let finalCrop;
+
 if (window.netlifyIdentity) {
   window.netlifyIdentity.on("login", (user) => {
     userEmail = user.email;
     document.getElementById("email").value = userEmail;
   });
 }
-
-
 
 function userCheck() {
   if (!netlifyIdentity.currentUser()) {
@@ -17,99 +55,209 @@ function userCheck() {
 document.addEventListener("DOMContentLoaded", userCheck);
 setInterval(userCheck, 2666);
 
-function calculate() {
-  // Get user input values
-  var landArea = parseFloat(document.getElementById("landArea").value);
-  var nitrogen = parseFloat(document.getElementById("nitrogen").value);
-  var phosphorous = parseFloat(document.getElementById("phosphorous").value);
-  var potassium = parseFloat(document.getElementById("potassium").value);
+function calculations() {
+  idealNitrogenForLand = idealNitrogen * landArea;
+  idealPhosphorousForLand = idealPhosphorous * landArea;
+  idealPotassiumForLand = idealPotassium * landArea;
 
-  // Define ideal nutrient levels
-  var idealNitrogen = 120;
-  var idealPhosphorous = 60;
-  var idealPotassium = 40;
+  totalNitrogen = nitrogen * landArea;
+  totalPhosphorous = phosphorous * landArea;
+  totalPotassium = potassium * landArea;
 
-  // Calculate ideal nutrient levels for land
-  var idealNitrogenForLand = Math.round(idealNitrogen * landArea);
-  var idealPhosphorousForLand = Math.round(idealPhosphorous * landArea);
-  var idealPotassiumForLand = Math.round(idealPotassium * landArea);
+  nitrogenDifference = idealNitrogenForLand - totalNitrogen;
+  phosphorousDifference = idealPhosphorousForLand - totalPhosphorous;
+  potassiumDifference = idealPotassiumForLand - totalPotassium;
+  
+  nitrogenFertilizer = nitrogenDifference / 0.46;
+  phosphorousFertilizer = phosphorousDifference / 0.46;
+  potassiumFertilizer = potassiumDifference / 0.5;
 
-  // Calculate total nutrients in land
-  var totalNitrogen = Math.round(nitrogen * landArea);
-  var totalPhosphorous = Math.round(phosphorous * landArea);
-  var totalPotassium = Math.round(potassium * landArea);
+  totalNitrogenCost = Math.round(nitrogenFertilizer * fertilizerCostNitrogen);
+  totalPhosphorousCost = Math.round(phosphorousFertilizer * fertilizerCostPhosphorous);
+  totalPotassiumCost = Math.round(potassiumFertilizer * fertilizerCostPotassium);
+  totalCost = totalNitrogenCost + totalPhosphorousCost + totalPotassiumCost;
+}
 
-  // Calculate nutrient differences
-  var nitrogenDifference = Math.round(idealNitrogenForLand - totalNitrogen);
-  var phosphorousDifference = Math.round(idealPhosphorousForLand - totalPhosphorous);
-  var potassiumDifference = Math.round(idealPotassiumForLand - totalPotassium);
+function wheat() {
+  idealNitrogen = 120;
+  idealPhosphorous = 60;
+  idealPotassium = 40;
+  calculations();
+}
 
-  // Calculate fertilizer requirements
-  var nitrogenFertilizer = Math.round(nitrogenDifference / 0.46);
-  var phosphorousFertilizer = Math.round(phosphorousDifference / 0.46);
-  var potassiumFertilizer = Math.round(potassiumDifference / 0.5);
+function mustard() {
+  idealNitrogen = 120;
+  idealPhosphorous = 60;
+  idealPotassium = 40;
+  calculations();
+}
 
-  // Calculate approximate cost per hectare
-  var fertilizerCostNitrogen = 60;
-  var fertilizerCostPhosphorous = 110;
-  var fertilizerCostPotassium = 100;
+function chickpea() {
+  idealNitrogen = 120;
+  idealPhosphorous = 60;
+  idealPotassium = 40;
+  calculations();
+}
 
-  //Calculate total cost for land
-  var totalNitrogenCost = Math.round(nitrogenFertilizer * fertilizerCostNitrogen);
-  var totalPhosphorousCost = Math.round(phosphorousFertilizer * fertilizerCostPhosphorous);
-  var totalPotassiumCost = Math.round(potassiumFertilizer * fertilizerCostPotassium);
-  var totalCost = Math.round(totalNitrogenCost + totalPhosphorousCost + totalPotassiumCost);
+function barley() {
+  idealNitrogen = 120;
+  idealPhosphorous = 60;
+  idealPotassium = 40;
+  calculations();
+}
 
-  // Update hidden fields
+function rice() {
+  idealNitrogen = 120;
+  idealPhosphorous = 60;
+  idealPotassium = 40;
+  calculations();
+}
+
+function maize() {
+  idealNitrogen = 120;
+  idealPhosphorous = 60;
+  idealPotassium = 40;
+  calculations();
+}
+
+function sugarcane() {
+  idealNitrogen = 120;
+  idealPhosphorous = 60;
+  idealPotassium = 40;
+  calculations();
+}
+
+function cotton() {
+  idealNitrogen = 120;
+  idealPhosphorous = 60;
+  idealPotassium = 40;
+  calculations();
+}
+
+function update() {
+  document.getElementById("finalCrop").textContent = finalCrop;
 
   document.getElementById("idealNitrogen").value = idealNitrogen;
   document.getElementById("idealPhosphorous").value = idealPhosphorous;
   document.getElementById("idealPotassium").value = idealPotassium;
 
   document.getElementById("nitrogenDifference").value = nitrogenDifference;
-  document.getElementById("phosphorousDifference").value =
-    phosphorousDifference;
+  document.getElementById("phosphorousDifference").value = phosphorousDifference;
   document.getElementById("potassiumDifference").value = potassiumDifference;
 
   document.getElementById("nitrogenFertilizer").value = nitrogenFertilizer;
-  document.getElementById("phosphorousFertilizer").value =
-    phosphorousFertilizer;
+  document.getElementById("phosphorousFertilizer").value = phosphorousFertilizer;
   document.getElementById("potassiumFertilizer").value = potassiumFertilizer;
 
   document.getElementById("totalNitrogenCost").value = totalNitrogenCost;
   document.getElementById("totalPhosphorousCost").value = totalPhosphorousCost;
   document.getElementById("totalPotassiumCost").value = totalPotassiumCost;
 
-  document.getElementById("totalCost").value = totalCost;
-  // Update displayed results in the separate div element
-  // document.getElementById("displayIdealNitrogen").textContent = idealNitrogen;
-  // document.getElementById("displayIdealPhosphorous").textContent = idealPhosphorous;
-  // document.getElementById("displayIdealPotassium").textContent = idealPotassium;
+  document.getElementById("totalCost").value = finalTotalCost;
 
-  document.getElementById("displayNitrogenDifference").textContent =
-    nitrogenDifference;
-  document.getElementById("displayPhosphorousDifference").textContent =
-    phosphorousDifference;
-  document.getElementById("displayPotassiumDifference").textContent =
-    potassiumDifference;
+  document.getElementById("displayNitrogenDifference").textContent = nitrogenDifference;
+  document.getElementById("displayPhosphorousDifference").textContent = phosphorousDifference;
+  document.getElementById("displayPotassiumDifference").textContent = potassiumDifference;
 
-  document.getElementById("displayNitrogenFertilizer").textContent =
-    Math.round(nitrogenFertilizer);
-  document.getElementById("displayPhosphorousFertilizer").textContent =
-    Math.round(phosphorousFertilizer);
-  document.getElementById("displayPotassiumFertilizer").textContent =
-    Math.round(potassiumFertilizer);
+  document.getElementById("displayNitrogenFertilizer").textContent = Math.round(nitrogenFertilizer);
+  document.getElementById("displayPhosphorousFertilizer").textContent = Math.round(phosphorousFertilizer);
+  document.getElementById("displayPotassiumFertilizer").textContent = Math.round(potassiumFertilizer);
 
-  document.getElementById("displayTotalNitrogenCost").textContent =
-    Math.round(totalNitrogenCost);
-  document.getElementById("displayTotalPhosphorousCost").textContent =
-    Math.round(totalPhosphorousCost);
-  document.getElementById("displayTotalPotassiumCost").textContent =
-    Math.round(totalPotassiumCost);
+  document.getElementById("displayTotalNitrogenCost").textContent = Math.round(totalNitrogenCost);
+  document.getElementById("displayTotalPhosphorousCost").textContent = Math.round(totalPhosphorousCost);
+  document.getElementById("displayTotalPotassiumCost").textContent = Math.round(totalPotassiumCost);
 
-  document.getElementById("displayTotalCost").textContent =
-    Math.round(totalCost);
+  document.getElementById("displayTotalCost").textContent = Math.round(finalTotalCost);
 
-  // Show the results container div
   document.getElementById("resultsContainer").style.display = "block";
+}
+
+function calculate() {
+  //state = document.getElementById("state").value;
+  season = document.getElementById("season").value;
+  landArea = parseFloat(document.getElementById("landArea").value);
+  phInput = parseFloat(document.getElementById("phInput").value);
+  moisture = parseFloat(document.getElementById("moisture").value);
+  nitrogen = parseFloat(document.getElementById("nitrogen").value);
+  phosphorous = parseFloat(document.getElementById("phosphorous").value);
+  potassium = parseFloat(document.getElementById("potassium").value);
+
+  if (season == "Rabi") {
+    wheat();
+    let val1 = totalCost;
+    mustard();
+    let val2 = totalCost;
+    chickpea();
+    let val3 = totalCost;
+    barley();
+    let val4 = totalCost;
+
+    finalTotalCost = Math.min(val1, val2, val3, val4);
+
+    if (finalTotalCost = val1) {
+      finalCrop = "Wheat";
+    }
+    else if (finalTotalCost = val2) {
+      finalCrop = "Mustard";
+    }
+    else if (finalTotalCost = val3) {
+      finalCrop = "Chickpea";
+    }
+    else if (finalTotalCost = val4) {
+      finalCrop = "Barley";
+    }
+  }
+
+  else if (season = "Kharif") {
+    rice();
+    let val1 = totalCost;
+    maize();
+    let val2 = totalCost;
+    sugarcane();
+    let val3 = totalCost;
+    cotton();
+    let val4 = totalCost;
+
+    finalTotalCost = Math.min(val1, val2, val3, val4);
+
+    if (finalTotalCost = val1) {
+      finalCrop = "Rice";
+    }
+    else if (finalTotalCost = val2) {
+      finalCrop = "Maize";
+    }
+    else if (finalTotalCost = val3) {
+      finalCrop = "Sugarcane";
+    }
+    else if (finalTotalCost = val4) {
+      finalCrop = "Cotton";
+    }
+  }
+
+  if (finalCrop = "Wheat") {
+    wheat();
+  }
+  else if (finalCrop = "Mustard") {
+    mustard();
+  }
+  else if (finalCrop = "Chickpea") {
+    chickpea();
+  }
+  else if (finalCrop = "Barley") {
+    barley();
+  }
+  else if (finalCrop = "Rice") {
+    rice();
+  }
+  else if (finalCrop = "Maize") {
+    maize();
+  }
+  else if (finalCrop = "Sugarcane") {
+    sugarcane();
+  }
+  else if (finalCrop = "Cotton") {
+    cotton();
+  }
+
+  update();
 }
