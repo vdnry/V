4c583db359a687b0020f56c7a554e383747baf16
RealOrangeIND <realorangeind@gmail.com>
2023-12-04 12:48:49 +0530

    Added essential elements to forum page

---
+++ forum/forumScript.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var toolsDropdown = document.getElementById("tools-dropdown");
+    var dropdownContent = document.getElementById("tools-dropdown-content");
+    toolsDropdown.addEventListener("click", function (event) {
+      dropdownContent.style.display =
+        dropdownContent.style.display === "block" ? "none" : "block";
+      event.stopPropagation();
+    });
+  
+    document.addEventListener("click", function (event) {
+      var isClickInsideDropdown =
+        toolsDropdown.contains(event.target) ||
+        dropdownContent.contains(event.target);
+  
+      if (!isClickInsideDropdown) {
+        dropdownContent.style.display = "none";
+      }
+    });
+  });
