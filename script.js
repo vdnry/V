2f389e929de966a29102031bf244b8b6bc4556f7
RealOrangeIND <realorangeind@gmail.com>
2023-12-04 12:34:14 +0530

    Made dropdown menu for touchscreen

--- script.js
+++ script.js
@@ -3,3 +3,23 @@ if (window.netlifyIdentity) {
     console.log("Logged in as:", user.email);
   });
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  var toolsDropdown = document.getElementById("tools-dropdown");
+  var dropdownContent = document.getElementById("tools-dropdown-content");
+  toolsDropdown.addEventListener("click", function (event) {
+    dropdownContent.style.display =
+      dropdownContent.style.display === "block" ? "none" : "block";
+    event.stopPropagation();
+  });
+
+  document.addEventListener("click", function (event) {
+    var isClickInsideDropdown =
+      toolsDropdown.contains(event.target) ||
+      dropdownContent.contains(event.target);
+
+    if (!isClickInsideDropdown) {
+      dropdownContent.style.display = "none";
+    }
+  });
+});
