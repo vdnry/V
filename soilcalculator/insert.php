ee0a673635ac2854e8b559e81486244c5b814715
RealOrangeIND <realorangeind@gmail.com>
2024-01-27 01:25:54 +0530

    Completely followed tutroial

--- soilcalculator/insert.php
+++ soilcalculator/insert.php
@@ -34,13 +34,29 @@ if (!empty($email) || !empty($selectedState) || !empty($currentDate) || !empty($
     if (mysqli_connect_error()) {
         die('Connect Error('.mysqli_connect_errno().')'.mysqli_connect_error());
     } else {
+        $SELECT = "SELECT email From history Where email = ? Limit 1";
         $INSERT = "INSERT Into history (email, selectedState, currentDate, landArea, phInput, tdsInput, nitrogen, phosphorous, potassium, season, idealNitrogen, idealPhosphourous, idealPotassium, nitrogenDifference, phosphorousDifference, potassiumDifference, nitrogenFertilizer, phosphorousFertilizer, potassiumFertilizer, totalNitrogenCost, totalPhosphorousCost, totalPotassiumCost, totalCost) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-        $stmt = $conn->prepare($INSERT);
-        $stmt->bind_param("sssiiiiiisiiiiiiiiiiiii", $email, $selectedState, $currentDate, $landArea, $phInput, $tdsInput, $nitrogen, $phosphorous, $potassium, $season, $idealNitrogen, $idealPhosphourous, $idealPotassium, $nitrogenDifference, $phosphorousDifference, $potassiumDifference, $nitrogenFertilizer, $phosphorousFertilizer, $potassiumFertilizer, $totalNitrogenCost, $totalPhosphorousCost, $totalPotassiumCost, $totalCost);
+        
+        $stmt = $conn->prepare($SELECT);
+        $stmt->bind_param("s", $email);
         $stmt->execute();
-        echo "New record inserted sucessfully";
+        $stmt->bind_result("$email");
+        $stmt->store_result();
+        $rnum = $stmt->num_rows();
+
+        if ($rnum==0) {
+            $stmt->close();
+
+            $stmt = $conn->prepare($INSERT);
+            $stmt->bind_param("sssiiiiiisiiiiiiiiiiiii", $email, $selectedState, $currentDate, $landArea, $phInput, $tdsInput, $nitrogen, $phosphorous, $potassium, $season, $idealNitrogen, $idealPhosphourous, $idealPotassium, $nitrogenDifference, $phosphorousDifference, $potassiumDifference, $nitrogenFertilizer, $phosphorousFertilizer, $potassiumFertilizer, $totalNitrogenCost, $totalPhosphorousCost, $totalPotassiumCost, $totalCost);
+            $stmt->execute();
+            echo "New record inserted sucessfully";
+        } else {
+            echo "Someone already registered using this email";
+        }
         $stmt->close();
         $conn->close();
+        
     }
 } else {
     echo "All fields are required";
